3a068d22da2c91973e38d655205326c7f4256d25
Veer Bajpai <veerbajpai839@gmail.com>
2026-09-23 14:59:35 +0530

    Time: 12 ms (76.63%) | Memory: 12.3 MB (58.88%) - LeetSync

---
+++ 1970-sorting-the-sentence/sorting-the-sentence.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def sortSentence(self, s):
+        splited_string = s[::-1].split()
+        splited_string.sort()
+
+        res = []
+
+        for word in splited_string:
+            res.append(word[1:][::-1])
+        return " ".join(res) 
